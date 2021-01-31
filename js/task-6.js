152492bf65df47a6db54bd91bce2f2b942518fc7
Zoia Apostoloska <zoiaapostoloska@gmail.com>
2021-01-31 23:47:25 +0200

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -15,9 +15,10 @@ const inputRef = document.querySelector('#validation-input');
 
 const checkSymbolQuantity = () => {
     if (inputRef.value.length !== Number(inputRef.dataset.length)) {
-        return inputRef.classList.add('invalid');
+        return inputRef.setAttribute('class', 'invalid');
     } 
-    inputRef.classList.add('valid');
+    inputRef.setAttribute('class', 'valid');
 }
 
 inputRef.addEventListener('blur', checkSymbolQuantity);
+
